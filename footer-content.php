ed900f13500e2d7b3ed01183052360f0298b4c98
secretimbecile <jackjkelly1506@gmail.com>
2017-02-19 17:50:01 +0000

    Changes for Jam

--- footer-content.php
+++ footer-content.php
@@ -1,4 +1,4 @@
 <footer>
-	<p><a href="https://github.com/SecretImbecile/pressandforget" target="_blank"><em>Press and Forget</em></a> WordPress theme by <a href="http://jackjkelly.com/" target="_blank">Jack Kelly</a></p>
-	<p>Proudly powered by <a href="https://wordpress.com/" target="_blank">WordPress</a>
+	<p><a href="https://github.com/McrRaspJam/acacia-acuminata" target="_blank"><em>Acacia Acuminata</em></a> WordPress theme by <a href="http://jackjkelly.com/" target="_blank">Jack Kelly</a>.
+	Manchester Raspberry Jam is part of the <a href="http://www.raspberryjam.org/">Raspberry Jam</a> network. We are supported and hosted by <a href="http://www.bytemark.co.uk/r/mcrraspjam" id="bytemark">Bytemark Hosting</a></p>
 </footer>
